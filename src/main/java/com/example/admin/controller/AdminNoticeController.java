77128d070136698e83b68de8177e29b779265aac
drssee <113401870+drssee@users.noreply.github.com>
2022-10-28 02:31:59 +0900

    Update AdminNoticeController.java

--- src/main/java/com/example/admin/controller/AdminNoticeController.java
+++ src/main/java/com/example/admin/controller/AdminNoticeController.java
@@ -15,6 +15,6 @@ public class AdminNoticeController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("AdminNoticeController.doGet");
-        req.getRequestDispatcher("notice.jsp");
+        req.getRequestDispatcher("notice.jsp").forward(req,resp);
     }
 }
