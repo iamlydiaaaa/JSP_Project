8cab73265e7cc93da890c9923709b892ea27601c
drssee <113401870+drssee@users.noreply.github.com>
2022-10-28 02:32:25 +0900

    Update UserMyPageController.java

--- src/main/java/com/example/user/controller/UserMyPageController.java
+++ src/main/java/com/example/user/controller/UserMyPageController.java
@@ -14,6 +14,6 @@ public class UserMyPageController extends UserController {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("userMyPageController.doGet");
-        req.getRequestDispatcher("myPage.jsp");
+        req.getRequestDispatcher("myPage.jsp").forward(req,resp);
     }
 }
